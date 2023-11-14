8c75b999ed27bffd4e23a87cf53e8ce925301caf
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-11-14 08:45:07 +0530

    Modifing Jenkinsfile Changes.

---
+++ main.tf
@@ -0,0 +1,56 @@
+variable "components" {
+  default = [
+    "frontend",
+    "cart",
+    "catalogue",
+    "user",
+    "payment",
+    "shipping",
+    "dispatch",
+    "mysql",
+    "rabbitmq",
+    "redis",
+    "mongodb",
+  ]
+}
+
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
+
+resource "aws_instance" "instance" {
+  count = length(var.components)
+  ami = data.aws_ami.ami.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-07afa043b24022e4e"]
+  tags = {
+    Name = element(var.components, count.index)
+  }
+}
+
+resource "aws_route53_record" "record" {
+  count = length(var.components)
+  zone_id = ""
+  name    = "${element(var.components, count.index)}-dev"
+  type    = "A"
+  ttl     = 30
+  records = [element(aws_instance.instance.*.private_ip, count.index)]
+}
+
+resource "null_resource" "set-hostname" {
+  count = length(var.components)
+  provisioner "remote-exec" {
+    connection {
+      host = element(aws_instance.instance.*.private_ip, count.index)
+      user = "root"
+      password = "DevOps321"
+    }
+    inline = "set-hostname ${element(var.components, count.index)}"
+  }
+}
+
