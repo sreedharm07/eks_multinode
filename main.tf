658a2a977e86864f68c31981b788c18c5eb2d07b
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-12-04 07:41:24 +0530

    [D75-7] | Raghu K | Terraform Code for RoboShop Project.

--- main.tf
+++ main.tf
@@ -66,64 +66,64 @@ module "rabbitmq" {
   bastion_node_cidr      = var.bastion_node_cidr
 }
 
-module "ms-components" {
-
-  depends_on = [module.docdb, module.mysql, module.elasticache, module.rabbitmq]
-
-  source = "git::https://github.com/raghudevopsb75/tf-module-app.git"
-
-  for_each               = var.components
-  component              = each.key
-  env                    = var.env
-  subnets                = module.vpc.db_subnets
-  vpc_cidr               = var.vpc_cidr
-  vpc_id                 = module.vpc.vpc_id
-  kms_key_id             = var.kms_key_id
-  instance_count         = each.value["count"]
-  prometheus_cidr        = var.prometheus_cidr
-  bastion_node_cidr      = var.bastion_node_cidr
-  instance_type          = each.value["instance_type"]
-  app_port               = each.value["app_port"]
-  alb_dns_name           = lookup(lookup(module.alb, each.value["lb_type"], null), "alb_dns_name", null)
-  zone_id                = "Z09059901XRPHNYMGLMJ4"
-  listener_arn           = lookup(lookup(module.alb, each.value["lb_type"], null), "listener_arn", null)
-  listener_rule_priority = each.value["listener_rule_priority"]
-}
-
-module "alb" {
-  source            = "git::https://github.com/raghudevopsb75/tf-module-alb.git"
-  for_each          = var.alb
-  alb_sg_allow_cidr = each.value["alb_sg_allow_cidr"]
-  alb_type          = each.key
-  env               = var.env
-  internal          = each.value["internal"]
-  subnets           = each.key == "private" ? module.vpc.app_subnets : module.vpc.public_subnets
-  vpc_id            = module.vpc.vpc_id
-  port              = each.value["port"]
-  protocol          = each.value["protocol"]
-  ssl_policy        = each.value["ssl_policy"]
-  certificate_arn   = each.value["certificate_arn"]
-}
-
-resource "aws_instance" "load-runner" {
-  ami                    = "ami-03265a0778a880afb"
-  instance_type          = "t3.medium"
-  vpc_security_group_ids = ["sg-07afa043b24022e4e"]
-  tags = {
-    Name = "load-runner"
-  }
-
-  provisioner "remote-exec" {
-    connection {
-      host     = self.private_ip
-      user     = "root"
-      password = "DevOps321"
-    }
-    inline = [
-      "curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/docker/install.sh | sudo bash",
-      "docker pull robotshop/rs-load:latest"
-    ]
-  }
-
-}
+#module "ms-components" {
+#
+#  depends_on = [module.docdb, module.mysql, module.elasticache, module.rabbitmq]
+#
+#  source = "git::https://github.com/raghudevopsb75/tf-module-app.git"
+#
+#  for_each               = var.components
+#  component              = each.key
+#  env                    = var.env
+#  subnets                = module.vpc.db_subnets
+#  vpc_cidr               = var.vpc_cidr
+#  vpc_id                 = module.vpc.vpc_id
+#  kms_key_id             = var.kms_key_id
+#  instance_count         = each.value["count"]
+#  prometheus_cidr        = var.prometheus_cidr
+#  bastion_node_cidr      = var.bastion_node_cidr
+#  instance_type          = each.value["instance_type"]
+#  app_port               = each.value["app_port"]
+#  alb_dns_name           = lookup(lookup(module.alb, each.value["lb_type"], null), "alb_dns_name", null)
+#  zone_id                = "Z09059901XRPHNYMGLMJ4"
+#  listener_arn           = lookup(lookup(module.alb, each.value["lb_type"], null), "listener_arn", null)
+#  listener_rule_priority = each.value["listener_rule_priority"]
+#}
+#
+#module "alb" {
+#  source            = "git::https://github.com/raghudevopsb75/tf-module-alb.git"
+#  for_each          = var.alb
+#  alb_sg_allow_cidr = each.value["alb_sg_allow_cidr"]
+#  alb_type          = each.key
+#  env               = var.env
+#  internal          = each.value["internal"]
+#  subnets           = each.key == "private" ? module.vpc.app_subnets : module.vpc.public_subnets
+#  vpc_id            = module.vpc.vpc_id
+#  port              = each.value["port"]
+#  protocol          = each.value["protocol"]
+#  ssl_policy        = each.value["ssl_policy"]
+#  certificate_arn   = each.value["certificate_arn"]
+#}
+#
+#resource "aws_instance" "load-runner" {
+#  ami                    = "ami-03265a0778a880afb"
+#  instance_type          = "t3.medium"
+#  vpc_security_group_ids = ["sg-07afa043b24022e4e"]
+#  tags = {
+#    Name = "load-runner"
+#  }
+#
+#  provisioner "remote-exec" {
+#    connection {
+#      host     = self.private_ip
+#      user     = "root"
+#      password = "DevOps321"
+#    }
+#    inline = [
+#      "curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/docker/install.sh | sudo bash",
+#      "docker pull robotshop/rs-load:latest"
+#    ]
+#  }
+#
+#}
 
