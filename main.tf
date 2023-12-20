c40a128d62a09898c0df783e4f72daf3c53e16e6
91707 <sreedhar70755@gmail.com>
2023-12-20 09:41:50 +0530

    tf

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
-module "vpc" {
-  source                 = "git::https://github.com/raghudevopsb75/tf-module-vpc.git"
-  vpc_cidr               = var.vpc_cidr
-  env                    = var.env
-  public_subnets         = var.public_subnets
-  web_subnets            = var.web_subnets
-  app_subnets            = var.app_subnets
-  db_subnets             = var.db_subnets
-  azs                    = var.azs
-  account_no             = var.account_no
-  default_vpc_id         = var.default_vpc_id
-  default_vpc_cidr       = var.default_vpc_cidr
-  default_route_table_id = var.default_route_table_id
-}
+#module "vpc" {
+#  source                 = "git::https://github.com/raghudevopsb75/tf-module-vpc.git"
+#  vpc_cidr               = var.vpc_cidr
+#  env                    = var.env
+#  public_subnets         = var.public_subnets
+#  web_subnets            = var.web_subnets
+#  app_subnets            = var.app_subnets
+#  db_subnets             = var.db_subnets
+#  azs                    = var.azs
+#  account_no             = var.account_no
+#  default_vpc_id         = var.default_vpc_id
+#  default_vpc_cidr       = var.default_vpc_cidr
+#  default_route_table_id = var.default_route_table_id
+#}
 #
 #
 #module "mysql" {
@@ -132,7 +132,7 @@ module "eks" {
 
   project_name   = var.project_name
   env            = var.env
-  subnet_ids     = module.vpc.app_subnets
+  subnet_ids     = ["subnet-0ef96b42e3c1f1f84", "subnet-0e9e3c82bec3a0d3a"]
   instance_types = var.eks_instance_types
   node_count     = var.eks_node_count
 }
